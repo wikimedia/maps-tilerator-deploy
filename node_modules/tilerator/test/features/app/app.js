fa3da236161c8c28d3c3897c21f50bdbe076c32e
Stephane Bisson <sbisson@wikimedia.org>
2018-04-10 14:07:51 -0400

    Update maps/tilerator/package to cb582ed
    List of changes:
    cb582ed Package v0.0.25 for deployment
    xxxxxxx Update node module dependencies
    
    Change-Id: I8b6c986ab4e060bb7b6ead77ce7aab92ade18013

--- node_modules/tilerator/test/features/app/app.js
+++ node_modules/tilerator/test/features/app/app.js
@@ -1,80 +1,58 @@
+/* global describe it before */
+
+// eslint-disable-next-line strict,lines-around-directive
 'use strict';
 
+const fs = require('fs');
+const preq = require('preq');
+const wait = require('wait-as-promised');
+const assert = require('../../utils/assert.js');
+const server = require('../../utils/server.js');
 
-var preq   = require('preq');
-var assert = require('../../utils/assert.js');
-var server = require('../../utils/server.js');
+function deleteIfExist(path) {
+  try {
+    fs.unlinkSync(path);
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      throw err;
+    }
+  }
+}
 
+function fileExists(file) {
+  try {
+    const stats = fs.statSync(file);
+    return stats.isFile();
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      throw err;
+    }
+  }
+  return false;
+}
 
-describe('express app', function() {
+describe('express app', function expressApp() {
+  this.timeout(20000);
 
-    this.timeout(20000);
+  before(() => server.start());
 
-    before(function () { return server.start(); });
+  it('should get robots.txt', () => preq.get({
+    uri: `${server.config.uri}robots.txt`,
+  }).then((res) => {
+    assert.deepEqual(res.status, 200);
+    assert.deepEqual(res.headers.disallow, '/');
+  }));
 
-    it('should get robots.txt', function() {
-        return preq.get({
-            uri: server.config.uri + 'robots.txt'
-        }).then(function(res) {
-            assert.deepEqual(res.status, 200);
-            assert.deepEqual(res.headers['disallow'], '/');
-        });
+  it('moves a tile from source to destination', () => {
+    // ensure file doesn't exist yet
+    deleteIfExist('test/filestore/6/33/22.png');
+    return preq.post({
+      uri: `${server.config.uri}add?generatorId=gen&storageId=file&zoom=6&x=33&y=22`,
+    }).then((res) => {
+      assert.deepEqual(res.status, 200);
+      assert.deepEqual(res.body[0], 'Z=6; 1 tile at [33,22] (idx=1577); gen→file');
+
+      return wait(() => fileExists('test/filestore/6/33/22.png'), { timeout: 15000 });
     });
-
-    it('should set CORS headers', function() {
-        if(server.config.service.conf.cors === false) {
-            return true;
-        }
-        return preq.get({
-            uri: server.config.uri + 'robots.txt'
-        }).then(function(res) {
-            assert.deepEqual(res.status, 200);
-            assert.deepEqual(res.headers['access-control-allow-origin'], '*');
-            assert.deepEqual(!!res.headers['access-control-allow-headers'], true);
-            assert.deepEqual(!!res.headers['access-control-expose-headers'], true);
-        });
-    });
-
-    it('should set CSP headers', function() {
-        if(server.config.service.conf.csp === false) {
-            return true;
-        }
-        return preq.get({
-            uri: server.config.uri + 'robots.txt'
-        }).then(function(res) {
-            assert.deepEqual(res.status, 200);
-            assert.deepEqual(res.headers['x-xss-protection'], '1; mode=block');
-            assert.deepEqual(res.headers['x-content-type-options'], 'nosniff');
-            assert.deepEqual(res.headers['x-frame-options'], 'SAMEORIGIN');
-            assert.deepEqual(res.headers['content-security-policy'], 'default-src');
-            assert.deepEqual(res.headers['x-content-security-policy'], 'default-src');
-            assert.deepEqual(res.headers['x-webkit-csp'], 'default-src');
-        });
-    });
-
-    it.skip('should get static content gzipped', function() {
-        return preq.get({
-            uri: server.config.uri + 'static/index.html',
-            headers: {
-                'accept-encoding': 'gzip, deflate'
-            }
-        }).then(function(res) {
-            // check that the response is gzip-ed
-            assert.deepEqual(res.headers['content-encoding'], 'gzip', 'Expected gzipped contents!');
-        });
-    });
-
-    it('should get static content uncompressed', function() {
-        return preq.get({
-            uri: server.config.uri + 'static/index.html',
-            headers: {
-                'accept-encoding': ''
-            }
-        }).then(function(res) {
-            // check that the response is gzip-ed
-            assert.deepEqual(res.headers['content-encoding'], undefined, 'Did not expect gzipped contents!');
-        });
-    });
-
+  });
 });
-
