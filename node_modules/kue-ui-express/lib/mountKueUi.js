52720d602fc5455423f722baa213affdbc87fe0f
Michael Holloway <mholloway@wikimedia.org>
2018-07-25 16:55:21 -0400

    Update maps/tilerator/package to eb7e9fb
    List of changes:
    eb7e9fb Bump deployment node version to 6.11.1
    xxxxxxx Update node module dependencies
    
    Change-Id: I9e0f764e4db9c719529113d31f1571984774edca

---
+++ node_modules/kue-ui-express/lib/mountKueUi.js
@@ -0,0 +1,35 @@
+const serveStatic = require('serve-static');
+const { JSDOM } = require('jsdom');
+const kueUiClient = require('kue-ui-client');
+
+const indexFile = kueUiClient.getIndexFile();
+
+function mountKueUi(app, route, apiURL, authmakerConfig) {
+  if (!route.endsWith('/')) {
+    throw new Error('You must end your route with /');
+  }
+
+  app.use(serveStatic(kueUiClient.getDistPath(), {
+    index: false,
+  }));
+
+  app.get(`${route}*`, (req, res) => {
+    const { window } = new JSDOM(indexFile);
+
+    const scriptEl = window.document.createElement('script');
+    scriptEl.text = `window.__kueUiExpress = {
+  rootUrl: "${route}",
+  apiURL: "${apiURL || '/kue-api'}"
+};`;
+
+    if (authmakerConfig) {
+      scriptEl.text += `window.__kueUiExpress.authmaker = ${JSON.stringify(authmakerConfig)}`;
+    }
+
+    window.document.body.appendChild(scriptEl);
+
+    res.send(window.document.documentElement.outerHTML);
+  });
+}
+
+module.exports = mountKueUi;
